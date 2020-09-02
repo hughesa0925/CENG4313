299b189597e3cdb7cdc3a39813c8ff8467e4baf1
hughesa0925 <hughesa0925@uhcl.edu>
2020-09-01 19:44:38 -0500

    Lab 2

---
+++ lab2/Objects/eventrecorder.d
@@ -0,0 +1,16 @@
+./objects/eventrecorder.o: \
+  C:\Users\18327\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.6.3\Source\EventRecorder.c \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\ARMCM4_FP.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\system_ARMCM4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Users\18327\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.6.3\Include\EventRecorder.h \
+  RTE\Compiler\EventRecorderConf.h
---
+++ lab2/blink.s
@@ -0,0 +1,67 @@
+IOCON_P1_13		EQU 0x4002C0B4 ; P1[13] IOCON_P1_13 R/W 0x030 0x4002C0B4
+IOCON_P1_18		EQU 0x4002C0C8 ; P1[18] IOCON_P1_18 R/W 0x030 0x4002 C0C8 D (tables 83, 84)
+	
+; This is essentially Table 94 from UM10562
+LPC4088QSB_P0	EQU 0x20098000
+LPC4088QSB_P1	EQU 0x20098020
+LPC4088QSB_P2	EQU 0x20098040
+LPC4088QSB_P3	EQU 0x20098060
+LPC4088QSB_P4	EQU 0x20098080
+LPC4088QSB_P5	EQU 0x200980A0
+DIR_REG_OFFSET	EQU 0x00
+MSK_REG_OFFSET	EQU 0x10
+PIN_REG_OFFSET	EQU 0x14
+SET_REG_OFFSET	EQU 0x18
+CLR_REG_OFFSET	EQU 0x1C
+; End Table 94
+bit13				EQU (1 << 13) ; 0x00002000
+bit18				EQU (1 << 18) ; 0x00040000
+	AREA my_asm, CODE, READONLY
+	ENTRY
+	EXPORT			__main
+__main
+	; setup LED#1 on port1[18]
+	; setup the, IOCON, I/O configuration - default is 0x30, setup to 0x00
+	; setup for GPIO and inactive and no hysteresis
+	LDR			R1, =IOCON_P1_18		 ; Pg 126
+	LDR			R2, =0x00000000
+	STR			R2,[R1,#0x00]
+	
+	LDR	R1,=LPC4088QSB_P1				; pointer to base reg of port1 - Pg 146
+	LDR R3,=bit18						; set LED 1 port pin to output - pin 18
+	STR R3,[R1,#DIR_REG_OFFSET]			; set DIRECTION bits
+	
+	LDR R1,=LPC4088QSB_P1				; pointer to base reg of port1
+	LDR R3,=0x00000000					; MSK register setting
+	STR R3,[R1,#MSK_REG_OFFSET] 		; set MASK bits
+	
+	;# LED is NEGATIVE logic,
+	;# SET turns it OFF
+	;# CLR turns it ON
+	LDR R1,=LPC4088QSB_P1				; pointer to base reg of port1
+	LDR R3,=bit18						; SET register setting
+	STR R3,[R1,#SET_REG_OFFSET] 		; set SET bits - This turns OFF the LED
+	
+	bl delay
+	
+	LDR R1,=LPC4088QSB_P1				; pointer to base reg of port1
+	LDR R3,=bit18						; CLR register setting
+	STR R3,[R1,#CLR_REG_OFFSET]			 ; set CLEAR bits - This turns ON the LED
+	
+stp
+	b stp
+	
+	
+; ------------------------------------------
+; Delay subroutine
+delay
+		PUSH {R0,LR}
+		MOVS r0, #0x1000000			;1 cycle
+B1
+		SUBS r0, r0, #1 			;1 cycle
+		BNE B1						;2 if taken, 1 otherwise
+		POP		{R0,PC}				; end delay subroutine
+; ------------------------------------------
+	ALIGN
+	END
+		
